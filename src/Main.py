883917ef756d9ab06bcf207332b5aff0ac4d658c
harry48225 <harrybest@live.co.uk>
2016-02-14 19:34:43 +0000

    Added Octopus Movement

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,39 @@
 import pygame
-from pygame.locals import *
+
+import time
+pygame.init()
 
 screen = pygame.display.set_mode((1280,720))
 
 octopus = pygame.image.load('8pi.png').convert()
-
-screen.blit(pygame.image.load('8pi.png').convert(), (100,10))
-
-raw_input()
+x = 0
+y = 0
+while True:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            
+            raise SystemExit
+    keypressed = pygame.key.get_pressed()
+    
+    if keypressed[pygame.K_w] and y > 0:
+        
+        y -= 0.1
+        
+    if keypressed[pygame.K_s] and y < 720:
+        
+        y += 0.1
+        
+    if keypressed[pygame.K_a] and x > 0:
+        
+        x -= 0.1
+        
+    if keypressed[pygame.K_d] and x < 1280:
+        
+        x += 0.1    
+    screen.fill((0,0,0))            
+    screen.blit(octopus, (x,y))
+    pygame.display.flip()
+                
+                
+                
