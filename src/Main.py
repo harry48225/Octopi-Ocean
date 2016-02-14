19111223d31e67279b5c2602bdd2582c77fab149
harry48225 <harrybest@live.co.uk>
2016-02-14 19:11:33 +0000

    fbdgdfg

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,10 @@
 import pygame
+from pygame.locals import *
 
+screen = pygame.display.set_mode((1280,720))
 
-print 'Hello world'
+octopus = pygame.image.load('8pi.png').convert()
+
+screen.blit(pygame.image.load('8pi.png').convert(), (100,10))
+
+raw_input()
